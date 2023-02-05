179838287ff22c593280426d11e868e029e63c63
WeSail <1192048641@qq.com>
2023-02-05 18:07:55 +0800

    sub 8 9 10

--- matlabcode/test.m
+++ matlabcode/test.m
@@ -1,1 +1,3 @@
-ProcOneAction('sub4','Run')
+configuration.fileName=["inms15k1.mat","inms15k2.mat","inms15k3.mat","outms15k1.mat","outms15k2.mat","outms15k3.mat"];
+configuration.actionName = ["Back_kick","Front_kick","Hop","Run","Side_leg_lift","Sit","Sitting_leg_extension","Squart","Stand","Stand_with_one_leg_back","Stand_with_one_leg_up","Tiptoe_stand","Walk"];
+save('configuration.mat',"configuration");
